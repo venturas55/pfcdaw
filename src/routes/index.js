5bda2e11fb9667872ae93f441369aa454d53da02
venturas55 <venturas55@hotmail.com>
2023-04-27 17:53:51 +0200

    Se añade web de error y sin permiso, así como sus routes. Se añaden las webs estaticas. se añade imagen de background y algunos styles.css

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const router = express.Router();
 const db = require("../database"); //db hace referencia a la BBDD
+const funciones = require("../lib/funciones.js");
 
 //MOSTRAR PAGINA INICIAL
 router.get('/', (req, res) => {
@@ -14,6 +15,65 @@ router.get('/profile',  (req, res) => {
 });
 router.get('/profile/edit',  (req, res) => {
     res.render('auth/profileEdit');
+});
+
+router.post('/profile/edit/', funciones.isAuthenticated, async(req, res) => {
+    const rows = await db.query("SELECT * FROM usuarios WHERE id= ?", [req.body.id]);
+    var user = rows[0];
+
+    console.log(req.body.oldcontrasena + " / " + user.contrasena);
+    const validPassword = await funciones.verifyPassword(req.body.oldcontrasena, user.contrasena);
+    if (validPassword) {
+        user.usuario = req.body.usuario;
+        user.email = req.body.email;
+        user.full_name = req.body.full_name;
+        user.contrasena = await funciones.encryptPass(req.body.newcontrasena);
+        console.log("guardando en la BBDD");
+        //console.log(user);
+        const result = await db.query("UPDATE usuarios SET ? where id= ?", [user, req.body.id]);
+        funciones.insertarLog(req.user.usuario, "UPDATE usuarios", "");
+        req.flash("success", "Usuario editado correctamente.");
+        res.redirect('/profile');
+    } else {
+        req.flash("warning", "No dispones de permisos!");
+        res.redirect('/noperm');
+    }
+});
+router.get("/profile/delete/:id", funciones.isAuthenticated, async(req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    //borramos foto
+    const filePath = path.resolve('src/public/img/profiles/' + user.pictureURL);
+    access(filePath, constants.F_OK, async(err) => {
+        if (err) {
+            console.log("No tiene foto de perfil");
+        } else {
+            console.log('File exists. Deleting now ...');
+            await unlink(filePath);
+        }
+    });
+    //hacemos logout
+    req.logOut();
+    const user = await db.query("SELECT * FROM usuarios where id=?", [id]);
+    //TODO: las pxtas sesiones parece que hay que borrarlas todas si no empieza a dar errores...
+    await db.query("DELETE FROM sessions");
+    await db.query("DELETE FROM usuarios WHERE id=?", [id]);
+
+    req.flash("success", "Usuario borrado correctamente");
+
+    res.redirect('/');
+});
+
+//MOSTRAR ERROR
+router.get('/error', (req, res) => {
+    console.log("Redirect");
+    req.flash("warning", "Sucedió algun error!");
+    res.render('error');
+});
+router.get('/noperm', (req, res) => {
+    console.log("Redirect");
+    req.flash("warning", "No dispones de los permisos adecuados!");
+    res.render('noPermission');
 });
 
 
