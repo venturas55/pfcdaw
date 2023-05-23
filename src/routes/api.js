d4a2f3c402b0b1842bbbd1a24e835de6c5b5e4ae
venturas55 <venturas55@hotmail.com>
2023-05-23 08:11:32 +0200

    Se separa la parte de pruebas de la API en el navbar. Se comenta api.js. Se añade un trigger a databases

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,9 @@ const { Passport } = require('passport');
 const router = express.Router();
 const pool = require("../database");
 const queryBaliza = "SELECT b.nif,lo.puerto,lo.num_local,b.telecontrol,b.periodo,b.tipo,b.apariencia,b.caracteristica,lo.localizacion,lo.latitud,lo.longitud,la.altura,la.elevacion,la.linterna,la.alcanceNom,la.alcanceLum,la.candelasCalc,la.candelasInst FROM balizamiento b  LEFT JOIN localizacion lo ON lo.nif=b.nif  LEFT JOIN lampara la ON la.nif=b.nif where b.nif=?";
+const queryListadoAton = "SELECT b.nif,b.num_internacional,b.tipo,b.apariencia,b.periodo,b.caracteristica,b.telecontrol,lo.puerto,lo.num_local,lo.localizacion,lo.latitud,lo.longitud,la.altura,la.elevacion,la.alcanceNom,la.linterna,la.candelasCalc,la.alcanceLum,la.candelasInst FROM balizamiento b  LEFT JOIN localizacion lo ON lo.nif=b.nif  LEFT JOIN lampara la ON la.nif=b.nif";
 
+//Devuelve el listado de todas las balizas introducidas
 router.get('/api/balizas',async (req,res)=>{
     const balizas = await pool.query(queryListadoAton);
     //const observaciones = await pool.query('SELECT * FROM observaciones where nif=?',[nif]);
@@ -11,6 +13,7 @@ router.get('/api/balizas',async (req,res)=>{
     res.send(balizas);
 });
 
+//Devuelve las caracteristicas de la baliza introducida 
 router.get('/api/baliza/:nif',async (req,res)=>{
     const { nif } = req.params;
     console.log(nif); 
@@ -22,6 +25,7 @@ router.get('/api/baliza/:nif',async (req,res)=>{
     res.send(baliza);
 });
 
+//Devuelve los mantenimientos de la baliza introducida 
 router.get('/api/mantenimiento/:nif',async (req,res)=>{
     const { nif } = req.params;
     console.log(nif); 
@@ -30,6 +34,7 @@ router.get('/api/mantenimiento/:nif',async (req,res)=>{
     res.send(q);
 });
 
+//Devuelve las observaciones de la baliza introducida 
 router.get('/api/observaciones/:nif',async (req,res)=>{
     const { nif } = req.params;
     console.log(nif); 
@@ -38,6 +43,7 @@ router.get('/api/observaciones/:nif',async (req,res)=>{
     res.send(q);
 });
 
+//Devuelve el listado de todos los usuarios
 router.get('/api/usuarios',async (req,res)=>{
     const balizas = await pool.query('SELECT full_name,usuario,email, privilegio FROM usuarios');
     //const observaciones = await pool.query('SELECT * FROM observaciones where nif=?',[nif]);
