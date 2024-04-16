87b178fd27724e48090e94b303383b88c5ba1b89
venturas55 <venturas55@hotmail.com>
2024-04-16 09:07:22 +0200

    Se eliminan copias de bbdd y se hace un nuevo sannuevo.sql con la estructura de la bbdd

---
+++ database/sannuevo2.sql
@@ -0,0 +1,166 @@
+drop database if exists sanpfcdaw;
+
+create database sanpfcdaw;
+
+use sanpfcdaw;
+
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+START TRANSACTION;
+
+SET
+  time_zone = "+00:00";
+
+/*INICIO informacion ATON*/
+CREATE TABLE IF NOT EXISTS `balizamiento` (
+  `nif` varchar(8) NOT NULL,
+  `num_internacional` varchar(12) DEFAULT NULL,
+  `tipo` varchar(250) DEFAULT NULL,
+  `telecontrol` varchar(200) DEFAULT NULL,
+  `apariencia` varchar(50) DEFAULT NULL,
+  `periodo` decimal(5, 2) DEFAULT NULL,
+  `caracteristica` varchar(50) DEFAULT NULL,
+  `necesita_pintado` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`nif`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de balizas';
+
+CREATE TABLE IF NOT EXISTS `lampara` (
+  `nif` varchar(8) NOT NULL,
+  `altura` decimal(5, 2) DEFAULT NULL,
+  `elevacion` decimal(5, 2) DEFAULT NULL,
+  `alcanceNom` decimal(5, 2) DEFAULT NULL,
+  `linterna` varchar(200) DEFAULT NULL,
+  `candelasCalc` float(12, 2) DEFAULT NULL,
+  `alcanceLum` decimal(5, 2) DEFAULT NULL,
+  `candelasInst` float(12, 2) DEFAULT NULL,
+  PRIMARY KEY (`nif`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de detalles lampara';
+
+CREATE TABLE IF NOT EXISTS `localizacion` (
+  `nif` varchar(8) NOT NULL,
+  `puerto` varchar(50) DEFAULT NULL,
+  `num_local` decimal(5, 0) DEFAULT NULL,
+  `localizacion` varchar(70) DEFAULT NULL,
+  `latitud` varchar(15) DEFAULT NULL,
+  `longitud` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`nif`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de localizacion del balizamiento';
+
+CREATE TABLE IF NOT EXISTS `mantenimiento` (
+  `id_mantenimiento` int(10) NOT NULL AUTO_INCREMENT,
+  `nif` varchar(8) NOT NULL,
+  `fecha` date NOT NULL,
+  `mantenimiento` varchar(250) NOT NULL,
+  PRIMARY KEY (`id_mantenimiento`),
+  KEY `mantenimiento_FK` (`nif`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de mantenimiento del balizamiento';
+
+CREATE TABLE IF NOT EXISTS `observaciones` (
+  `id_observacion` int(10) NOT NULL AUTO_INCREMENT,
+  `nif` varchar(8) NOT NULL,
+  `observaciones` varchar(250) NOT NULL,
+  PRIMARY KEY (`id_observacion`),
+  KEY `observaciones_FK` (`nif`)
+) ENGINE = InnoDB AUTO_INCREMENT = 188 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de observaciones del balizamiento';
+
+/*FIN ATON*/
+/* INVENTARIO */
+CREATE TABLE IF NOT EXISTS `inventario` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(50) DEFAULT NULL,
+  `item` varchar(50) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `fila` int(11) DEFAULT NULL,
+  `columna` int(11) DEFAULT NULL,
+  `descripcion` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 79 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de inventario';
+
+
+/* DESCRIPCION DOCUMENTOS */
+CREATE TABLE `documentos` (
+  `id_archivo` varchar(100) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(250) DEFAULT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'tabla de documentos';
+
+/* USUARIOS y LOGS */
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `contrasena` varchar(250) NOT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `full_name` varchar(150) DEFAULT NULL,
+  `privilegio` varchar(20) DEFAULT NULL,
+  `pictureURL` varchar(100) CHARACTER SET utf16 COLLATE utf16_spanish2_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de usuarios';
+
+CREATE TABLE IF NOT EXISTS `logs` (
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `usuario` varchar(50) NOT NULL,
+  `accion` varchar(100) DEFAULT NULL,
+  `observacion` varchar(150) DEFAULT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de logs';
+
+drop table if exists tickets;
+CREATE TABLE IF NOT EXISTS `tickets` (
+  `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nif` varchar(8) DEFAULT NULL,
+  `created_by_id` int(11) NOT NULL,
+  `assigned_to_id` int(11) DEFAULT NULL,
+  `resolved_by_id` int(11) DEFAULT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `solved_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`ticket_id`),
+  KEY `created_by_id` (`created_by_id`),
+  KEY `assigned_to_id` (`assigned_to_id`),
+  KEY `resolved_by_id` (`resolved_by_id`),
+  KEY `nif` (`nif`),
+  CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`created_by_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `tickets_ibfk_2` FOREIGN KEY (`assigned_to_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`resolved_by_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `tickets_ibfk_4` FOREIGN KEY (`nif`) REFERENCES `balizamiento` (`nif`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'tabla de tickets';
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE `tokens` (
+  `user_id` int(11) NOT NULL,
+  `hashedtoken` varchar(200) PRIMARY KEY,
+  `expires` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+   FOREIGN KEY (user_id) REFERENCES usuarios(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'tabla de tokens';
+
+-- Inserta usuario "admin" con contraseña "admin"
+INSERT INTO
+  `usuarios` (
+    `id`,
+    `usuario`,
+    `contrasena`,
+    `email`,
+    `full_name`,
+    `privilegio`,
+    `pictureURL`
+  )
+VALUES
+  (
+    1,
+    'admin',
+    '$2a$10$44RiEqgdwBZhtbd1rN6pfe/CLbTMpc4mGUPDiCgAlle0ISkMuJAC2',
+    'admin@email.com',
+    'Admin name',
+    'admin',
+    ''
+  );
+
+COMMIT;
--- src/public/dumpSQL/pruebas.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO
-  `mantenimiento` (`id_mantenimiento`, `nif`, `fecha`, `mantenimiento`) VALUES  (
-    1188,
-    '25565',
-    '2023-12-09',
-    'Se repintó en concepto de 2023 Nuñez dirá fecha...'
-  );
