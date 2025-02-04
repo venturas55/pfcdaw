152bbb06269ca626421f4a44755d9594d88eaf47
venturas55 <venturas55@hotmail.com>
2025-02-04 08:59:07 +0100

    Se añade modificación de contraseña a parte con validacion en cliente y servidor

--- src/public/js/validaciones.js
+++ src/public/js/validaciones.js
@@ -3,14 +3,13 @@ var checkPass = function() {
     if (document.getElementById('password').value ==
       document.getElementById('confirmPassword').value) {
       document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'passwords matching';
+      document.getElementById('message').innerHTML = 'Las contraseñas coinciden';
       document.getElementById('btn-setpass').classList.remove('disabled');
   
     } else {
       document.getElementById('message').style.color = 'red';
       document.getElementById('confirmPassword').style.color = 'red'
-      document.getElementById('message').innerHTML = 'Please make sure your passwords match';
-      console.log("hago disabled");
+      document.getElementById('message').innerHTML = 'Otro gañan, quieres poner bien las contraseñas!.';
       document.getElementById('btn-setpass').classList.add('disabled');
     }
   }
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -26,22 +26,20 @@ router.post('/profile/edit', funciones.isAuthenticated, async (req, res) => {
     const rows = await db.query("SELECT * FROM usuarios WHERE id= ?", [req.body.id]);
     var user = rows[0];
 
-    console.log(req.body.oldcontrasena + " / " + user.contrasena);
-    const validPassword = await funciones.verifyPassword(req.body.oldcontrasena, user.contrasena);
-    if (validPassword) {
-        user.usuario = req.body.usuario;
-        user.email = req.body.email;
-        user.full_name = req.body.full_name;
-        user.contrasena = await funciones.encryptPass(req.body.newcontrasena);
-        //console.log("guardando en la BBDD");
-        //console.log(user);
-        const result = await db.query("UPDATE usuarios SET ? where id= ?", [user, req.body.id]);
-        req.flash("success", "Usuario editado correctamente.");
-        res.redirect('/profile');
-    } else {
+
+    user.usuario = req.body.usuario;
+    user.email = req.body.email;
+    user.full_name = req.body.full_name;
+    //user.contrasena = await funciones.encryptPass(req.body.newcontrasena);
+    //console.log("guardando en la BBDD");
+    //console.log(user);
+    const result = await db.query("UPDATE usuarios SET ? where id= ?", [user, req.body.id]);
+    req.flash("success", "Usuario editado correctamente.");
+    res.redirect('/profile');
+    /* } else {
         req.flash("warning", "No has puesto la contraseña actual correctamente!");
         res.redirect('/noperm');
-    }
+    } */
 });
 router.get("/profile/delete/:id", funciones.isAuthenticated, async (req, res) => {
     console.log(req.params);
@@ -82,6 +80,36 @@ router.post('/doAdmin', funciones.isAuthenticated, async (req, res) => {
         funciones.insertarLog(req.user.usuario, "UPDATE usuarios", "Se le añade permisos de admin");
         res.redirect('/profile');
     } else {
+        res.redirect('/noperm');
+    }
+});
+router.get('/profile/changepass', async (req, res) => {
+    const { id } = req.params;
+    res.render("auth/changepass", { id });
+});
+router.post('/profile/changepass', async (req, res) => {
+
+    const { newcontrasena, newcontrasena2, oldcontrasena, id } = req.body;
+    //console.log(password + " "+ id);
+    const rows = await db.query("SELECT * FROM usuarios WHERE id= ?", [req.body.id]);
+    var user = rows[0];
+    console.log(oldcontrasena + " / " + user.contrasena);
+    const validPassword = await funciones.verifyPassword(oldcontrasena, user.contrasena);
+    if (validPassword) {
+        if (newcontrasena === newcontrasena2) {
+            var encryptedPass = await funciones.encryptPass(newcontrasena);
+            console.log("guardando en la BBDD");
+            console.log(newcontrasena + " " + encryptedPass);
+            await db.query("UPDATE usuarios set contrasena=? where id=?", [encryptedPass, id]);
+            req.flash("success", "Contraseña actualizada correctamente");
+            res.redirect("/profile/edit");
+        } else {
+            req.flash("warning", "Las contraseñas no coinciden");
+            res.redirect("/profile/edit");
+        }
+
+    } else {
+        req.flash("warning", "No has puesto la contraseña actual correctamente!");
         res.redirect('/noperm');
     }
 });
