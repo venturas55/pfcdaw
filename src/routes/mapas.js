a46fce61ba0804cb71f719839811aedc67e6376a
venturas <venturas55@hotmail.com>
2025-02-21 23:01:45 +0100

    se añade leaflet como tipo de mapa para usuarios sin logar

--- src/routes/mapas.js
+++ src/routes/mapas.js
@@ -3,17 +3,19 @@ const router = express.Router();
 import funciones from "../lib/funciones.js";
 const queryListadoAton = "SELECT b.nif,b.num_internacional,b.tipo,b.apariencia,b.periodo,b.caracteristica,b.telecontrol,b.necesita_pintado,lo.puerto,lo.num_local,lo.localizacion,lo.latitud,lo.longitud,la.altura,la.elevacion,la.alcanceNom,la.linterna,la.candelasCalc,la.alcanceLum,la.candelasInst FROM balizamiento b  LEFT JOIN localizacion lo ON lo.nif=b.nif  LEFT JOIN lampara la ON la.nif=b.nif";
 import db from "../database.js"; //db hace referencia a la BBDD
-
+var tipoMapa="Leaflet";
 //GESTION MAPAS
 router.get("/mapa/:nif", async (req, res) => {
     const { nif } = req.params;
+    (typeof req.user==="undefined") ? tipoMapa="Leaflet": tipoMapa=req.user.prefmap;
     const baliza = await db.query(queryListadoAton + ' where b.nif=?', [nif]);
-    res.render("mapas/mapa"+req.user.prefmap, { layout: 'layoutMapa'+req.user.prefmap, baliza: baliza[0] });
+    res.render("mapas/mapa"+tipoMapa, { layout: 'layoutMapa'+tipoMapa, baliza: baliza[0] });
 });
 //funcion get para mostrar los mapas dinamicos con la api de google maps
 router.get("/mapaGeneral/:valor", (req, res) => {
     //const { valor } = req.params;
-    res.render("mapas/mapa"+req.user.prefmap, { layout: 'layoutMapa'+req.user.prefmap });
+    (typeof req.user==="undefined") ? tipoMapa="Leaflet": tipoMapa=req.user.prefmap;
+    res.render("mapas/mapa"+tipoMapa, { layout: 'layoutMapa'+tipoMapa });
 });
 //funcion get para mostrar los mapas estaticos
 router.get("/mapaGeneral2/:valor", (req, res) => {
