19db3f5693871cb16a36e2d051e48344334eef1c
venturas55 <venturas55@hotmail.com>
2025-05-12 10:49:02 +0200

    Se añade un alter table en preventivos para posibilitar NULL en las fechas.. que se usa para mostrar unas u otras fichas de preventivos

--- database/addNewMods.sql
+++ database/addNewMods.sql
@@ -1,3 +1,5 @@
 alter table usuarios add column prefmap varchar(10) default 'Leaflet'; 
 alter table balizamiento add column apagada boolean default false; 
 alter table balizamiento add column esBoya boolean default false; 
+ALTER TABLE preventivos MODIFY fecha TIMESTAMP NULL;
+ALTER TABLE preventivos MODIFY solved_at TIMESTAMP NULL;
