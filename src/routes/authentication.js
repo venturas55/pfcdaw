1f31a1a52704d9c278dce568d413eaf9dafcd4cc
venturas55 <venturas55@hotmail.com>
2025-05-23 09:54:42 +0200

    Se deja como APV

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -45,14 +45,14 @@ router.get('/logout', (req, res) => {
 
 //GESTION recuperacion contraseña
 router.get('/profile/email/recordarpass/', async (req, res) => {
-    console.log(join(__dirname,'../public/img/ccby.png'));
     res.render('auth/recoverypass');
 });
 router.post('/profile/email/recordarpass/', async (req, res) => { //:email
 
     var transporter;
-    //console.log("EMAIL_AUTH_NEEDED: " + config.EMAIL_AUTH_NEEDED);
-    if (config.EMAIL_AUTH_NEEDED) {
+    console.log("EMAIL_AUTH_NEEDED: " + config.EMAIL_AUTH_NEEDED);
+    if (config.EMAIL_AUTH_NEEDED == "true") {
+        console.log("IF con AUTH")
         transporter = createTransport({
             //service: config.EMAIL_SERVICE,
             host: config.EMAIL_HOST,
@@ -65,19 +65,27 @@ router.post('/profile/email/recordarpass/', async (req, res) => { //:email
             secureConnection: false, // TLS requires secureConnection to be false
             attachments: [{
                 filename: 'ccby.png',
-                path: join(__dirname,'../public/img/ccby.png'),
-                cid: 'ccby' 
+                path: join(__dirname, '../public/img/ccby.png'),
+                cid: 'ccby'
             }]
-            /*   tls: {
-                  ciphers: 'SSLv3'
-              } */
+
         });
     } else {
+        let seguridad;
+        if (config.EMAIL_PORT == 465)
+            seguridad = true;
+        else
+            seguridad = false;
+        console.log(`Intentando enviar email con la siguiente configuracion \n \t host: ${config.EMAIL_HOST} \n \t port:  ${config.EMAIL_PORT} \n \t secure:  ${config.EMAIL_SECURITY}`);
         transporter = createTransport({
             //service: config.EMAIL_SERVICE,
             host: config.EMAIL_HOST,
             port: config.EMAIL_PORT,
-            secure: true,
+            secure: seguridad,
+            /*    tls: {
+                   rejectUnauthorized: false // (opcional) si es un servidor que usa TLS autofirmado
+                         ciphers: 'SSLv3'
+               } */
             //secureConnection: false, // TLS requires secureConnection to be false
         });
     }
@@ -92,8 +100,7 @@ router.post('/profile/email/recordarpass/', async (req, res) => { //:email
 
     const email = req.body.email;
     const usuario = req.body.usuario;
-    //var rows = await db.query("SELECT * FROM usuarios WHERE usuario = ? AND email= ?", [usuario, email]);
-    var rows = await db.query("SELECT * FROM usuarios WHERE email= ?", [email]);
+    var rows = await db.query("SELECT * FROM usuarios WHERE usuario = ? AND email= ?", [usuario, email]);
     if (rows.length > 0) {
         var user = rows[0];
         const user_id = user.id;
@@ -113,73 +120,73 @@ router.post('/profile/email/recordarpass/', async (req, res) => { //:email
             to: email,
             subject: 'Restablecer contraseña BBDD SAN',
             html: ` 
-            <!DOCTYPE html>
-            <html lang="es">
-                <head>
-                <meta charset="UTF-8">
-                <title>Restablecer Contraseña</title>
-                </head>
-                <body style="background-color: #f8f9fa; padding: 20px; font-family: Arial, sans-serif; color: #212529;">
-                    <table align="center" width="100%" cellpadding="0" cellspacing="0" style="max-width: 600px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05); padding: 30px;">
-                        <tr>
-                        <td align="center" style="padding-bottom: 20px;">
-                            <h2 style="margin: 0; font-size: 24px;">Restablece tu contraseña ${user.usuario}</h2>
-                        </td>
-                        </tr>
-                        <tr>
-                        <td style="padding-bottom: 20px; font-size: 16px; line-height: 1.5;">
-                            Hola, hemos recibido una solicitud para restablecer tu contraseña. Si tú no realizaste esta solicitud, puedes ignorar este correo.
-                        </td>
-                        </tr>
-                        <tr>
-                        <td style="padding-bottom: 30px; font-size: 16px; line-height: 1.5;">
-                            Para cambiar tu contraseña, haz clic en el siguiente botón:
-                        </td>
-                        </tr>
-                        <tr>
-                        <td align="center" style="padding-bottom: 30px;">
-                            <a href="http://${req.headers.host}/profile/email/verifypass/${user_id}/${token}" 
-                            style="background-color: #0d6efd; color: #ffffff; padding: 12px 24px; text-decoration: none; border-radius: 4px; font-size: 16px; display: inline-block;">
-                            Restablecer contraseña
-                            </a>
-                        </td>
+                    <!DOCTYPE html>
+                    <html lang="es">
+                        <head>
+                        <meta charset="UTF-8">
+                        <title>Restablecer Contraseña</title>
+                        </head>
+                        <body style="background-color: #f8f9fa; padding: 20px; font-family: Arial, sans-serif; color: #212529;">
+                            <table align="center" width="100%" cellpadding="0" cellspacing="0" style="max-width: 600px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.05); padding: 30px;">
+                                <tr>
+                                <td align="center" style="padding-bottom: 20px;">
+                                    <h2 style="margin: 0; font-size: 24px;">Restablece tu contraseña ${user.usuario}</h2>
+                                </td>
+                                </tr>
+                                <tr>
+                                <td style="padding-bottom: 20px; font-size: 16px; line-height: 1.5;">
+                                    Hola, hemos recibido una solicitud para restablecer tu contraseña. Si tú no realizaste esta solicitud, puedes ignorar este correo.
+                                </td>
+                                </tr>
+                                <tr>
+                                <td style="padding-bottom: 30px; font-size: 16px; line-height: 1.5;">
+                                    Para cambiar tu contraseña, haz clic en el siguiente botón:
+                                </td>
+                                </tr>
+                                <tr>
+                                <td align="center" style="padding-bottom: 30px;">
+                                    <a href="http://${req.headers.host}/profile/email/verifypass/${user_id}/${token}" 
+                                    style="background-color: #0d6efd; color: #ffffff; padding: 12px 24px; text-decoration: none; border-radius: 4px; font-size: 16px; display: inline-block;">
+                                    Restablecer contraseña
+                                    </a>
+                                </td>
 
-                        </tr>
-                        <tr>
-                        <td style="font-size: 14px; color: #6c757d;">
-                            Este enlace es válido por 24 horas. Si tienes problemas, copia y pega esta URL en tu navegador:
-                            <br><br>
-                            <a href="http://${req.headers.host}/profile/email/verifypass/${user_id}/${token}" style="color: #0d6efd; word-break: break-all;">
-                            http://${req.headers.host}/profile/email/verifypass/${user_id}/${token}
-                            </a>
-                        </td>
-                        </tr>
-                        <tr>
-                        <td style="padding-bottom: 20px; font-size: 16px; line-height: 1.5;">
-                        Recuerda que tu usuario es ${user.usuario} 
-                        </td>
-                        </tr>
-                        <tr>
-                        <td style="padding-top: 30px; font-size: 12px; color: #adb5bd; text-align: center;">
-                        <span>Autoridad Portuaria de Valencia 2025</span>
-                            <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/">
-                                <img alt="Creative Commons License" src="cid:ccby"/>
-                            </a><span >Autor: </span> <a target="_blank" style="text-decoration:none;" href="https://adriandeharo.es">Adrian de Haro </a>
-                            <span style="display: inline-block;transform: rotate(180deg);"> &copy; </span>
-                            <span> bajo licencia</span> <a rel="license" target="_blank" style="text-decoration:none;" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons.</a>
-                        </td>
-                        </tr>
-                    </table>
-                </body>
-            </html>
-            `
+                                </tr>
+                                <tr>
+                                <td style="font-size: 14px; color: #6c757d;">
+                                    Este enlace es válido por 24 horas. Si tienes problemas, copia y pega esta URL en tu navegador:
+                                    <br><br>
+                                    <a href="http://${req.headers.host}/profile/email/verifypass/${user_id}/${token}" style="color: #0d6efd; word-break: break-all;">
+                                    http://${req.headers.host}/profile/email/verifypass/${user_id}/${token}
+                                    </a>
+                                </td>
+                                </tr>
+                                <tr>
+                                <td style="padding-bottom: 20px; font-size: 16px; line-height: 1.5;">
+                                Recuerda que tu usuario es ${user.usuario} 
+                                </td>
+                                </tr>
+                                <tr>
+                                <td style="padding-top: 30px; font-size: 12px; color: #adb5bd; text-align: center;">
+                                <span>Autoridad Portuaria de Valencia 2025</span>
+                                    <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by/4.0/">
+                                        <img alt="Creative Commons License" src="cid:ccby"/>
+                                    </a><span >Autor: </span> <a target="_blank" style="text-decoration:none;" href="https://adriandeharo.es">Adrian de Haro </a>
+                                    <span style="display: inline-block;transform: rotate(180deg);"> &copy; </span>
+                                    <span> bajo licencia</span> <a rel="license" target="_blank" style="text-decoration:none;" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons.</a>
+                                </td>
+                                </tr>
+                            </table>
+                        </body>
+                    </html>
+                    `
             //TODO: LA URL DEL SERVIDOR DESPLEGADO
         };
         transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.error("Error:");
                 console.log(error);
-                req.flash("error", "Error al enviar el eMail para restablecer contraseña")
+                req.flash("error", "Error al enviar el eMail para restablecer contraseña:", `host: ${config.EMAIL_HOST} \n \t port:  ${config.EMAIL_PORT} \n \t secure:  ${config.EMAIL_SECURITY}`, error)
                 res.redirect("/error");
 
             } else {
@@ -202,13 +209,13 @@ router.get('/profile/email/verifypass/:user_id/:code', async (req, res) => {
     const { user_id, code } = req.params;
     await db.query("DELETE FROM tokens WHERE expires < NOW()");
     var [token] = await db.query("SELECT * FROM tokens WHERE user_id=? ", [user_id]);
-    //console.log(token);
+    console.log(token);
     //  localhost:5001/profile/email/verifypass/1/1ZEFGJ      hashedtoken
     //console.log(code + " == " + token.hashedtoken);
     if (token) {
 
         const validToken = await funciones.verifyPassword(code, token.hashedtoken)
-        //console.log(validToken);
+        console.log(validToken);
         if (validToken) {
             req.flash("success", "Token proporcionado correcto");
             res.redirect("/profile/recoverysetpass/" + user_id);
